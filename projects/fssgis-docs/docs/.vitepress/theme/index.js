0c215dba4a3fac776ba90987c3b50bae93f7fd09
xizher <xizher@163.com>
2021-08-17 18:43:17 +0800

    feat(docs): 组件自动注册实现

--- projects/fssgis-docs/docs/.vitepress/theme/index.js
+++ projects/fssgis-docs/docs/.vitepress/theme/index.js
@@ -1,31 +1,17 @@
 import DefaultTheme from 'vitepress/theme'
-import StatisticsBox from '../../components/StatisticsBox.vue'
-import CreateGuid from '../../components/fssgis-utils/CreateGuid.vue'
-import CreateIntRandom from '../../components/fssgis-utils/CreateIntRandom.vue'
-import FssgBoxesExample from '../../components/fssgis-grid/FssgBoxesExample.vue'
-import FssgGridExample from '../../components/fssgis-grid/FssgGridExample.vue'
-import FssgLeafletQuickStartGuide from '../../components/fssg-leaflet/QuickStartGuide.vue'
-import EsriBeginToStart from '../../components/fssg-esri/EsriBeginToStart.vue'
-import EsriContainer from '../../components/fssg-esri/EsriContainer.vue'
-import ExampleUseZoom from '../../components/fssg-esri-hooks/ExampleUseZoom.vue'
-import ExampleUseCenter from '../../components/fssg-esri-hooks/ExampleUseCenter.vue'
-
 import { FssgIcon, FssgIconFont } from '@fssgis/ui'
+
+const modules = import.meta.glob('../../components/**/*.vue')
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }) {  
-    app.component('StatisticsBox', StatisticsBox)
-    app.component('CreateGuid', CreateGuid)
-    app.component('CreateIntRandom', CreateIntRandom)
-    app.component('FssgBoxesExample', FssgBoxesExample)
-    app.component('FssgGridExample', FssgGridExample)
-    app.component('FssgLeafletQuickStartGuide', FssgLeafletQuickStartGuide)
+  enhanceApp({ app }) {
+    Object.entries(modules).map(async ([path, fn]) => {
+      const _ = await fn()
+      app.component(path.split('.vue')[0].split('/')[path.split('.vue')[0].split('/').length - 1], _.default)
+    })
+
     app.component('FssgIcon', FssgIcon)
     app.component('FssgIconFont', FssgIconFont)
-    app.component('EsriBeginToStart', EsriBeginToStart)
-    app.component('ExampleUseZoom', ExampleUseZoom)
-    app.component('EsriContainer', EsriContainer)
-    app.component('ExampleUseCenter', ExampleUseCenter)
   }
 }
